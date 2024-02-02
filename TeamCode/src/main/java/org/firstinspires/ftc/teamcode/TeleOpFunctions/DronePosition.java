902a864636b54c64044134f3200a73b150188b67
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2024-02-01 21:37:30 -0500

    rEADY FOR TOURAMENT caps lock

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFunctions/DronePosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFunctions/DronePosition.java
@@ -13,7 +13,7 @@ public class DronePosition extends ServoControl
 
     public void PositionControl()
     {
-        super.driveServo(0.85, gamepad1.dpad_up);
+        super.driveServo(0.83, gamepad1.dpad_up);
         super.driveServo(0.7, gamepad1.dpad_down);
         if (gamepad1.dpad_left && gamepad1.b)
         {
